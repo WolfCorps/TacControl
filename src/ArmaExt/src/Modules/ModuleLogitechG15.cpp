20c3f6e532139e6f43bfa4df2f31d59bb4ccc148
Dedmen Miller <dedmen@dedmen.de>
2020-11-08 17:47:14 +0100

    Fixed ModuleLogitechG15 freezing main thread on init

--- src/ArmaExt/src/Modules/ModuleLogitechG15.cpp
+++ src/ArmaExt/src/Modules/ModuleLogitechG15.cpp
@@ -35,6 +35,48 @@ static bool (*LogiLcdMonoSetText)(int lineNumber, const wchar_t* text);
 
 
 void ModuleLogitechG15::Run() {
+
+    char path[MAX_PATH];
+    HMODULE hm = NULL;
+
+    if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+        GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+        (LPCSTR)&RVExtension, &hm) == 0)
+    {
+        int ret = GetLastError();
+        fprintf(stderr, "GetModuleHandle failed, error = %d\n", ret);
+        // Return or however you want to handle an error.
+    }
+    if (GetModuleFileName(hm, path, sizeof(path)) == 0)
+    {
+        int ret = GetLastError();
+        fprintf(stderr, "GetModuleFileName failed, error = %d\n", ret);
+        // Return or however you want to handle an error.
+    }
+
+
+    std::string dllPath = (std::filesystem::path(path).parent_path() / "LogitechLcd.dll").lexically_normal().string();
+
+    auto logiDLL = LoadLibraryExA(dllPath.c_str(), nullptr, 0);
+
+    if (!logiDLL) return;
+
+    LogiLcdInit = reinterpret_cast<decltype(LogiLcdInit)>(GetProcAddress(logiDLL, "LogiLcdInit"));
+    LogiLcdIsConnected = reinterpret_cast<decltype(LogiLcdIsConnected)>(GetProcAddress(logiDLL, "LogiLcdIsConnected"));
+    LogiLcdIsButtonPressed = reinterpret_cast<decltype(LogiLcdIsButtonPressed)>(GetProcAddress(logiDLL, "LogiLcdIsButtonPressed"));
+    LogiLcdUpdate = reinterpret_cast<decltype(LogiLcdUpdate)>(GetProcAddress(logiDLL, "LogiLcdUpdate"));
+    LogiLcdShutdown = reinterpret_cast<decltype(LogiLcdShutdown)>(GetProcAddress(logiDLL, "LogiLcdShutdown"));
+    LogiLcdMonoSetBackground = reinterpret_cast<decltype(LogiLcdMonoSetBackground)>(GetProcAddress(logiDLL, "LogiLcdMonoSetBackground"));
+    LogiLcdMonoSetText = reinterpret_cast<decltype(LogiLcdMonoSetText)>(GetProcAddress(logiDLL, "LogiLcdMonoSetText"));
+
+    LogiLcdInit(L"TacControl", LOGI_LCD_TYPE_MONO);
+
+    if (!LogiLcdIsConnected(LOGI_LCD_TYPE_MONO)) return;
+
+
+
+
+
     std::vector<bool> isTransmitting(4, false);
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
@@ -121,47 +163,5 @@ void ModuleLogitechG15::Run() {
 
 
 void ModuleLogitechG15::ModuleInit() {
-
-    char path[MAX_PATH];
-    HMODULE hm = NULL;
-
-    if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
-        GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-        (LPCSTR)&RVExtension, &hm) == 0)
-    {
-        int ret = GetLastError();
-        fprintf(stderr, "GetModuleHandle failed, error = %d\n", ret);
-        // Return or however you want to handle an error.
-    }
-    if (GetModuleFileName(hm, path, sizeof(path)) == 0)
-    {
-        int ret = GetLastError();
-        fprintf(stderr, "GetModuleFileName failed, error = %d\n", ret);
-        // Return or however you want to handle an error.
-    }
-
-
-
-
-    std::string dllPath = (std::filesystem::path(path).parent_path() / "LogitechLcd.dll").lexically_normal().string();
-
-    auto logiDLL = LoadLibraryExA(dllPath.c_str(), nullptr, 0);
-
-    if (!logiDLL) return;
-
-    LogiLcdInit = reinterpret_cast<decltype(LogiLcdInit)>(GetProcAddress(logiDLL, "LogiLcdInit"));
-    LogiLcdIsConnected = reinterpret_cast<decltype(LogiLcdIsConnected)>(GetProcAddress(logiDLL, "LogiLcdIsConnected"));
-    LogiLcdIsButtonPressed = reinterpret_cast<decltype(LogiLcdIsButtonPressed)>(GetProcAddress(logiDLL, "LogiLcdIsButtonPressed"));
-    LogiLcdUpdate = reinterpret_cast<decltype(LogiLcdUpdate)>(GetProcAddress(logiDLL, "LogiLcdUpdate"));
-    LogiLcdShutdown = reinterpret_cast<decltype(LogiLcdShutdown)>(GetProcAddress(logiDLL, "LogiLcdShutdown"));
-    LogiLcdMonoSetBackground = reinterpret_cast<decltype(LogiLcdMonoSetBackground)>(GetProcAddress(logiDLL, "LogiLcdMonoSetBackground"));
-    LogiLcdMonoSetText = reinterpret_cast<decltype(LogiLcdMonoSetText)>(GetProcAddress(logiDLL, "LogiLcdMonoSetText"));
-
-    LogiLcdInit(L"TacControl", LOGI_LCD_TYPE_MONO);
-
-    if (!LogiLcdIsConnected(LOGI_LCD_TYPE_MONO)) return;
-
-
     Thread::ModuleInit();
-
 }
