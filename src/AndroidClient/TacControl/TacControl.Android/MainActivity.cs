27353304fdf088b0e1e1d47a91dfa1f64920408d
Dedmen Miller <dedmen@dedmen.de>
2021-03-21 14:20:43 +0100

    Android aquire Wifi lock while TacControl is open

--- src/AndroidClient/TacControl/TacControl.Android/MainActivity.cs
+++ src/AndroidClient/TacControl/TacControl.Android/MainActivity.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Net;
 using Android.Net.Wifi;
 using Android.Runtime;
 using Android.Views;
@@ -16,6 +18,7 @@ using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Distribute;
 using SkiaSharp;
 using SkiaSharp.Views.Android;
+using TacControl.Common;
 using TacControl.Common.Modules;
 using Xamarin.Essentials;
 
@@ -25,16 +28,31 @@ namespace TacControl.Droid
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         private WifiManager.MulticastLock castLock;
+        private WifiManager.WifiLock wifiLock;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             AppCenter.Start("b17f9c9d-e90c-488f-8c4b-92ef3e305c0d", typeof(Analytics), typeof(Distribute));
 
 
             WifiManager wifiMgr = (WifiManager) ApplicationContext.GetSystemService(Context.WifiService);
+            wifiLock = wifiMgr.CreateWifiLock(WifiMode.Full, "TacControl-udp");
+            wifiLock.SetReferenceCounted(true);
+            wifiLock.Acquire();
             castLock = wifiMgr.CreateMulticastLock("TacControl-udp");
             castLock.SetReferenceCounted(true);
             castLock.Acquire();
 
+
+            ConnectivityManager conMgr = (ConnectivityManager)ApplicationContext.GetSystemService(Context.ConnectivityService);
+            var stuff = conMgr.GetAllNetworks();
+            var wifiNet = stuff.First(x => conMgr.GetNetworkInfo(x).Type == ConnectivityType.Wifi);
+
+            var res = conMgr.BindProcessToNetwork(wifiNet);
+            var info = conMgr.GetNetworkInfo(wifiNet);
+
+            var connInfo = wifiMgr.ConnectionInfo;
+
+            //Networking.ConnectionInfo
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
