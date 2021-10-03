8097bfb869b91ef2039cd1c4f77a6f8448c71fa4
Dedmen Miller <dedmen@dedmen.de>
2021-10-03 16:25:08 +0200

    Add ACE attachable GPS tracker

--- src/ArmaScript/addons/main/CfgEventHandlers.hpp
+++ src/ArmaScript/addons/main/CfgEventHandlers.hpp
@@ -15,3 +15,11 @@ class Extended_PostInit_EventHandlers {
         init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
+
+class Extended_Init_EventHandlers {
+    class TC_ACE_Attach_GPSTracker {
+        class TC_InitGPSTracker {
+            init = "[{[0,_this select 0,0] call TC_main_fnc_GPS_activateTracker}, _this] call CBA_fnc_execNextFrame;";
+        };
+    };
+};
